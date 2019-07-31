4a92ae3d97ffc6409db65a4378b242c9cff31702
juandonito <anguadir@gmail.com>
2019-07-31 16:41:49 +0200

    Adding icons and moving assets

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,13 +2,31 @@ import './NavBar.css'
 
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { ReactComponent as Star } from '../assets/icons/star.svg'
+import { ReactComponent as Clock } from '../assets/icons/clock.svg'
+import { ReactComponent as Heart } from '../assets/icons/heart.svg'
 
 const NavBar = () => {
+
+    const iconStyle = {
+        height: '24px',
+        width: '24px'
+    }
+
     return (
         <nav className='nav-bar'>
-            <Link to='/top-artists'>Top Artists</Link>
-            <Link to='/top-hits'>Top Hits</Link>
-            <Link to='/top-recent'>Top Recent</Link>
+            <Link to='/top-artists'>
+                <Star style={iconStyle} />
+                Top Artists
+            </Link>
+            <Link to='/top-hits'>
+                <Heart style={iconStyle} />
+                Top Hits
+            </Link>
+            <Link to='/top-recent'>
+                <Clock style={iconStyle} />
+                Top Recent
+            </Link>
         </nav>
     )
 }
